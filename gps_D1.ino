59a758426dcf8f42333d28d2cf572c81c0a145b2
Adel <adi.stein@outlook.cz>
2025-02-02 19:00:11 +0100

    GPS and vib senzor

---
+++ gps_D1.ino
@@ -0,0 +1,31 @@
+#include <SoftwareSerial.h>
+#include <TinyGPS++.h>
+
+#define RX_PIN 0 // D7 (ESP8266 RX)
+#define TX_PIN 1 // D6 (ESP8266 TX)
+
+SoftwareSerial gpsSerial(RX_PIN, TX_PIN); 
+TinyGPSPlus gps;
+
+void setup() {
+    Serial.begin(115200);
+    gpsSerial.begin(9600);
+    Serial.println("Čekám na GPS data...");
+
+}
+
+void loop() {
+    while (gpsSerial.available()) {
+        gps.encode(gpsSerial.read()); 
+
+        if (gps.location.isUpdated()) {
+            Serial.print("Latitude: ");
+            Serial.println(gps.location.lat(), 6);
+            Serial.print("Longitude: ");
+            Serial.println(gps.location.lng(), 6);
+            Serial.print("Altitude: ");
+            Serial.println(gps.altitude.meters());
+        }
+    }
+}
+//musím přepájet 
---
+++ vib_senzor.ino
@@ -0,0 +1,24 @@
+#define VIB_SENSOR D2  //GPIO4
+
+int vibThreshold = 800;  // Počet detekcí než uznáme silnou vibraci
+int vibCount = 0;
+
+void setup() {
+    pinMode(VIB_SENSOR, INPUT);
+    Serial.begin(115200);
+    Serial.println("Senzor připraven - detekuje silné rány...");
+}
+
+void loop() {
+    if (digitalRead(VIB_SENSOR) == HIGH) {
+        vibCount++;  
+
+        if (vibCount >= vibThreshold) {
+            Serial.println(" Silné bouchnutí detekováno!");
+            vibCount = 0;  
+        }
+    } else {
+        vibCount = 0;  
+    }
+    delay(20); 
+}
